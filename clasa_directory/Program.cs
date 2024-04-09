5ab26d00393536f20d6708025157318696668fcf
Noah23Mautner <152974783+Noah23Mautner@users.noreply.github.com>
2024-04-09 12:19:10 +0200

    Add files via upload

---
+++ clasa_directory/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clasa_directory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // unos putanje mape
+            Console.WriteLine("Unesi putanju mape");
+            string putanja = Console.ReadLine();
+            // Provjera dali mapa postoji
+            if (Directory.Exists(putanja))
+            {
+                Console.WriteLine("Mapa postoji!");
+
+
+
+                // ako postoji ispisuje sve datoteke
+                foreach (string datoteka in Directory.GetFiles(putanja))
+                {
+                    Console.WriteLine(datoteka);
+                }
+
+                
+            }
+            else
+            {
+                Directory.CreateDirectory(putanja);
+                Console.WriteLine("Mapa" + putanja + "je napravljen");
+            }
+
+            string putanjaPodmape = putanja + "\\bla";
+
+            if (!Directory.Exists(putanjaPodmape))
+            {
+                Directory.CreateDirectory(putanjaPodmape);
+                Console.WriteLine("Mapa" + putanjaPodmape + "je napravljen");
+            }
+            Console.WriteLine("Ispis Podmapa:");
+            foreach(string podmapa in Directory.GetDirectories(putanja))
+            {
+                Console.WriteLine(podmapa);
+            }
+            
+            Directory.Delete(putanjaPodmape);
+            Console.WriteLine("Ispis podmapa nakon brisanja:");
+
+            foreach (string poddirektorij in Directory.GetDirectories(putanja))
+            {
+                Console.WriteLine(poddirektorij);
+            }
+
+
+           
+            Console.ReadKey();
+        }
+    }
+}
